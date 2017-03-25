db355230f13a4462157217bf47a8eb1a039d9fd8
Chris Mitchell <cgmrain@gmail.com>
2017-03-25 14:10:53 -0700

    Initial Commit

---
+++ mapkittut/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mapkittut
+//
+//  Created by Chris Mitchell on 3/25/17.
+//  Copyright © 2017 Chris Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
